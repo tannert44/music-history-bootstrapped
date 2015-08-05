ab39b08dc1795c9cf471cecfdb33a9040d406787
Tanner <tannert8155@gmail.com>
2015-08-05 15:59:22 -0500

    Added to select box with handlebars

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,95 +13,69 @@ requirejs.config({
 });
 
 requirejs(
-  ["jquery","hbs","bootstrap","matchHeight","dom-access", "populate-songs", "get-more-songs"], 
+  ["jquery", "hbs", "bootstrap", "matchHeight", "dom-access", "populate-songs", "get-more-songs"],
   function($, Handlebars, bootstrap, matchHeight, dom, pop, getMore) {
-    
+
     pop.getSongs(function(data) {
+      
+      require(['hbs!../templates/artist'] , function(artistTemplate){
+        var populatedTemplate = artistTemplate(data);
+        $('.artist-select').html(populatedTemplate);
+       }); 
+      
+      require(['hbs!../templates/album'] , function(artistTemplate){
+        var populatedTemplate = artistTemplate(data);
+        $('.album-select').html(populatedTemplate);
+       }); 
+            
+      
+      
       require(['hbs!../templates/songs'], function(songTemplate) {
         dom.html(songTemplate(data))
-        $('.delete_button').on("click", function(){
-                   $(this).prev().remove();
-                   $(this).remove();
-                   $('.match-height').matchHeight();
-                  });
-        
+        $('.delete_button').on("click", function() {
+          $(this).prev().remove();
+          $(this).remove();
+          $('.match-height').matchHeight();
+        });
+
         $('.song-info-content').append("<button id='more_button'>More</button>");
-        
-          $('#more_button').on("click", function(){
-              getMore.getSongs(function(data) {
-               require(['hbs!../templates/songs'], function(songTemplate) {
-               $('#more_button').before(songTemplate(data))
-               $('.match-height').matchHeight();
-               $('.delete_button').on("click", function(){
-                   $(this).prev().remove();
-                   $(this).remove();
-                   $('.match-height').matchHeight();
-                });
-               });
+
+        $('#more_button').on("click", function() {
+
+          getMore.getSongs(function(data) {
+            
+            require(['hbs!../templates/artist'] , function(artistTemplate){
+        var populatedTemplate = artistTemplate(data);
+        $('.artist-select').append(populatedTemplate);
+       }); 
+            
+            require(['hbs!../templates/album'] , function(artistTemplate){
+        var populatedTemplate = artistTemplate(data);
+        $('.album-select').append(populatedTemplate);
+       }); 
+           
+            
+            require(['hbs!../templates/songs'], function(songTemplate) {
+              $('#more_button').before(songTemplate(data))
+              $('.match-height').matchHeight();
+              
+              $('.delete_button').on("click", function() {
+                $(this).prev().remove();
+                $(this).remove();
+                $('.match-height').matchHeight();
               });
+              
+            });
           });
+
+        });
       });
-//      var getSongs = data;
-//          $.each(getSongs, function(){
-//            var songOutput = "<h3 class='.new-item song-title'>";
-//                songOutput += this.name;
-//                songOutput += "<small>";
-//                songOutput += " - by ";
-//                songOutput += this.artist;
-//                songOutput += " on the album ";
-//                songOutput += this.album;
-//                songOutput += "</small>";
-//                songOutput += "</h3>";
-//                songOutput += "<div class='.new-item delete_button'><button class='btn btn-default btn-xs'>Delete</button></div>";
-//            dom.append(songOutput);
-//            
-//            
-//            $('.delete_button').on("click", function(){
-//                   $(this).prev().remove();
-//                   $(this).remove();
-//                   $('.match-height').matchHeight();
-//                  });
-//            
-//            
-//          });
-//      
-//      
-//          $('.song-info-content').append("<button id='more_button'>More</button>");
-//      
-//      
-//          getMore.getSongs(function(data2) {
-//            var getMoreSongs = data2;
-//            console.log(getMoreSongs);
-//      
-//             $('#more_button').on("click", function(){
-//               $.each(getMoreSongs, function(){
-//                 var songOutput2 = "<h3 class='song-title'>";
-//                 songOutput2 += this.name;
-//                 songOutput2 += "<small>";
-//                 songOutput2 += " - by ";
-//                 songOutput2 += this.artist;
-//                 songOutput2 += " on the album ";
-//                 songOutput2 += this.album;
-//                 songOutput2 += "</small>";
-//                 songOutput2 += "</h3>";
-//                 songOutput2 +="<div class='delete_button'><button class='btn btn-default btn-xs'>Delete</button></div>";
-//                $('#more_button').before(songOutput2);
-//                 
-//                 $('.match-height').matchHeight();
-//
-//                $('.delete_button').on("click", function(){
-//                   $(this).prev().remove();
-//                   $(this).remove();
-//                   $('.match-height').matchHeight();
-//                   });
-//               });
-//             });
-//            
-//          });
-        
+
+
     });
-    
+
     $('.match-height').matchHeight();
     
-  
-});
+    
+  });
+
